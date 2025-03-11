bd9a9f446a7178d29aa53c10208253c010b57472
Ashutosh Sharma <asrma.sharma@gmail.com>
2025-03-11 03:53:40 -0400

    Fixed backend env and enabled standalone output

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8089",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
